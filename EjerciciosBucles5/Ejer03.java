c7e595d060a5b2c99ad3f8ef7ed8865f08a0307f
magnitopic <magnitopic@gmail.com>
2022-10-07 10:23:35 +0200

    changed file names

--- EjerciciosBucles5/Ejer03.java
+++ EjerciciosBucles5/Ejer03.java
@@ -2,7 +2,7 @@ package EjerciciosBucles5;
 
 import java.util.Scanner;
 
-public class Ejer3 {
+public class Ejer03 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num, numCount = 0;
