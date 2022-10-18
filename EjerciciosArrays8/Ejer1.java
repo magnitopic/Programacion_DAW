0b5a0145a7220196ca7dc1fd15d8adaeccb92b3c
magnitopic <magnitopic@gmail.com>
2022-10-18 19:30:25 +0200

    EjerciciosArrays8 finished

--- EjerciciosArrays8/Ejer1.java
+++ EjerciciosArrays8/Ejer1.java
@@ -4,20 +4,19 @@ import java.util.Arrays;
 
 public class Ejer1 {
 	public static void main(String[] args) {
-		int [] numeros = {10,4,456,5,12,34,22,9,0,56,2,122,3};
+		int[] numeros = { 10, 4, 456, 5, 12, 34, 22, 9, 0, 56, 2, 122, 3 };
 		int aux;
-		
+
 		for (int i = 0; i < numeros.length; i++) {
-			for (int j = 0; j < numeros.length-1; j++) {
+			for (int j = 0; j < numeros.length - 1; j++) {
 				if (numeros[j] > numeros[j + 1]) {
 					aux = numeros[j];
 					numeros[j] = numeros[j + 1];
 					numeros[j + 1] = aux;
 				}
-				
 			}
 		}
 
-		System.out.println( Arrays.toString(numeros));
+		System.out.println(Arrays.toString(numeros));
 	}
 }
---
+++ EjerciciosArrays8/Ejer2.java
@@ -0,0 +1,13 @@
+package EjerciciosArrays8;
+
+import java.util.Arrays;
+
+public class Ejer2 {
+	public static void main(String[] args) {
+		int[] numeros = { 10, 4, 456, 5, 12, 34, 22, 9, 0, 56, 2, 122, 3 };
+
+		Arrays.sort(numeros);
+
+		System.out.println(Arrays.toString(numeros));
+	}
+}
