8af1ebb5095e476526ffad9e062ee2bda770b98d
magnitopic <magnitopic@gmail.com>
2022-10-19 13:24:02 +0200

    EjerciciosArrays9

---
+++ EjerciciosArrays9/Ejer01.java
@@ -0,0 +1,33 @@
+package EjerciciosArrays9;
+
+import java.util.Arrays;
+
+public class Ejer01 {
+	public static void main(String[] args) {
+		int[] nums = new int[25];
+		int[] aux = new int[25];
+		boolean flag = true;
+		int contador = 0;
+
+		for (int i = 0; i < nums.length; i++)
+			nums[i] = (int) ((Math.random() * 100) + 1);
+
+		for (int j = 0; j < nums.length; j++) {
+			flag = true;
+			for (int i = 2; i < nums[j]; i++) {
+				if (nums[j] % i == 0) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag)
+				aux[contador++] = nums[j];
+		}
+		int[] primes = new int[contador];
+		for (int i = 0; aux[i] != 0; i++)
+			primes[i] = aux[i];
+
+		System.out.println(Arrays.toString(primes));
+
+	}
+}
