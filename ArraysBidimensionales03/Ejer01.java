09d2ae09179db614fb974cc2be0d00e5ad1466cd
magnitopic <magnitopic@gmail.com>
2022-11-08 23:35:41 +0100

    new file structure

--- ArraysBidimensionales03/Ejer01.java
+++ ArraysBidimensionales03/Ejer01.java
@@ -1,4 +1,4 @@
-package ArraysBidimensionales02;
+package ArraysBidimensionales03;
 
 import java.util.Scanner;
 
@@ -7,6 +7,7 @@ public class Ejer01 {
 		Scanner sc = new Scanner(System.in);
 		Scanner sn = new Scanner(System.in);
 		String[][] datos = new String[2][6];
+		String[] subjects = { "redes", "sistemas", "móviles", "programación", "fol" };
 		boolean flag = false;
 		String name;
 
@@ -70,6 +71,68 @@ public class Ejer01 {
 					}
 					break;
 				case 6:
+					double sum = 0;
+					System.out.print("Introduce el nombre de la asignatura de la que quieres ver la media: ");
+					name = sn.nextLine();
+					flag = false;
+					for (int i = 0; i < subjects.length; i++) {
+						for (int j = 0; j < datos.length; j++) {
+							if (name.equalsIgnoreCase(subjects[i])) {
+								sum += Double.parseDouble(datos[j][i]);
+								flag = true;
+							}
+						}
+					}
+					if (!flag)
+						System.out.println("No se ha encontrado esa asignatura.");
+					else
+						System.out.println("La media de las notas es de " + (sum / subjects.length));
+					break;
+				case 7:
+					double min = Double.MAX_VALUE;
+					int pos = 0;
+					for (int i = 0; i < datos.length; i++) {
+						sum = 0;
+						for (int j = 1; j < datos[i].length; j++)
+							sum += Double.parseDouble(datos[i][j]);
+						if (sum < min) {
+							min = sum;
+							pos = i;
+						}
+					}
+					System.out.println("El estudiante con la media más baja es " + datos[pos][0]);
+					break;
+				case 8:
+					double max = Double.MIN_VALUE;
+					pos = 0;
+					for (int i = 0; i < datos.length; i++) {
+						sum = 0;
+						for (int j = 1; j < datos[i].length; j++)
+							sum += Double.parseDouble(datos[i][j]);
+						if (sum < max) {
+							max = sum;
+							pos = i;
+						}
+					}
+					System.out.println("El estudiante con la media más alta es " + datos[pos][0]);
+					break;
+				case 9:
+					int counter = 0;
+					max = 0;
+					pos = 0;
+					for (int i = 0; i < datos.length; i++) {
+						for (int j = 1; j < datos[i].length; j++) {
+							if (Double.parseDouble(datos[i][j]) < 5)
+								counter++;
+						}
+						if (counter > max) {
+							max = counter;
+							pos = i;
+						}
+					}
+					System.out.println("El estudiante con la media más alta es " + datos[pos][0]);
+					break;
+				case 10:
 					sc.close();
 					sn.close();
 					return;
@@ -104,7 +167,11 @@ public class Ejer01 {
 		System.out.println("3.	Modificar datos de un alumno");
 		System.out.println("4.	Poner un 0 a todos los alumnos en todos los módulos");
 		System.out.println("5.	Aprobado general a todos los alumnos en todos los módulos");
-		System.out.println("6.	Salir");
+		System.out.println("6.	Mostar en pantalla la media de una asignaura pedida al usuario");
+		System.out.println("7.	Mostrar el nombre del alumno con media más baja");
+		System.out.println("8.	Mostrar el nombre del alumno con media más alta");
+		System.out.println("9.	Mostrar el nombre del alumno con más nº de suspensos");
+		System.out.println("10.	Salir");
 		System.out.print("--> ");
 		opt = sc.nextInt();
 		return opt;
