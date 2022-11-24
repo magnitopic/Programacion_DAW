3ec79e27638559487c763e3f5415730af040d8ce
magnitopic <magnitopic@gmail.com>
2022-11-24 22:13:02 +0100

    Main3 clases StarWars

---
+++ StarWars/Main3.java
@@ -0,0 +1,93 @@
+package StarWars;
+
+public class Main3 {
+	public static void main(String[] args) {
+
+	}
+
+	private static int[] fuerzasJedis(Jedi[] jedis) {
+		int[] fuerzas = new int[jedis.length];
+
+		for (int i = 0; i < jedis.length; i++)
+			fuerzas[i] = jedis[i].getFuerza();
+		return fuerzas;
+	}
+
+	private static void nombresEdadFu(int edadMin, int edadMax, int fuerza, Padawan[] padawans) {
+		for (int i = 0; i < padawans.length; i++) {
+			int edad = padawans[i].getEdad();
+			if ((edad > edadMin && edad < edadMax) && padawans[i].getFuerza() >= fuerza)
+				System.out.println(padawans[i].getNombre());
+		}
+	}
+
+	private static void incrementoPotencial(int valor, Padawan[] padawans) {
+		Padawan padawan = padawans[(int) ((Math.random() * padawans.length) - 1)];
+		padawan.setFuerza(padawan.getFuerza() + valor);
+	}
+
+	private static String[] esmirriados(Padawan[] padawans) {
+		String[] esmirriados = new String[6];
+		Padawan[] padawnsEsmirriados = new Padawan[6];
+		for (int i = 0; i < padawans.length; i++) {
+			for (int j = 0; j < esmirriados.length; j++) {
+				if (padawnsEsmirriados[j] == null || padawans[i].getFuerza() > padawnsEsmirriados[j].getFuerza()) {
+					padawnsEsmirriados[j] = padawans[i];
+					esmirriados[j] = Integer.toString(padawnsEsmirriados[j].getFuerza());
+					break;
+				}
+			}
+		}
+		return esmirriados;
+	}
+
+	private static String[] variosNombres(Padawan[] padawans, Jedi[] jedis, int fuerza) {
+		int counter = 0;
+		for (int i = 0; i < jedis.length; i++) {
+			if (jedis[i].getFuerza() > fuerza)
+				counter++;
+		}
+		for (int i = 0; i < padawans.length; i++) {
+			if (padawans[i].getFuerza() > fuerza)
+				counter++;
+		}
+
+		String[] nombres = new String[counter];
+		for (int i = 0; i < jedis.length; i++) {
+			if (jedis[i].getFuerza() > fuerza) {
+				counter--;
+				nombres[counter] = jedis[i].getNombre();
+			}
+		}
+		for (int i = 0; i < padawans.length; i++) {
+			if (padawans[i].getFuerza() > fuerza) {
+				counter--;
+				nombres[counter] = padawans[i].getNombre();
+			}
+		}
+		return nombres;
+	}
+
+	private static void degradacipmFuerza(int valor, Jedi[] jedis) {
+		Jedi jedi = jedis[(int) ((Math.random() * jedis.length) - 1)];
+		jedi.setFuerza(jedi.getFuerza() - valor);
+	}
+
+	private static Padawan[] padawansFuertacos(Padawan[] padawans, int fuerza) {
+		int counter = 0;
+		for (int i = 0; i < padawans.length; i++) {
+			if (padawans[i].getFuerza() > fuerza)
+				counter++;
+		}
+
+		Padawan[] resultado = new Padawan[counter];
+
+		for (int i = 0; i < padawans.length; i++) {
+			if (padawans[i].getFuerza() > fuerza) {
+				counter--;
+				resultado[counter] = padawans[i];
+			}
+		}
+		return resultado;
+	}
+}
