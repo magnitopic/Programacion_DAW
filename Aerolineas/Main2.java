0ea5e9c537ff576a75d0a77ed2b18548ec2e1247
magnitopic <magnitopic@gmail.com>
2022-12-08 12:47:30 +0100

    Aerolineas prt5

--- Aerolineas/Main2.java
+++ Aerolineas/Main2.java
@@ -1,6 +1,7 @@
 package Aerolineas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main2 {
 
@@ -77,5 +78,53 @@ public class Main2 {
 				}
 			}
 		}
+	}
+
+	public static boolean checkDNI(ArrayList<Pasajero> pasajeros, String dni) {
+		for (int i = 0; i < pasajeros.size(); i++) {
+			if (pasajeros.get(i).getDni().equals(dni))
+				return true;
+		}
+		return false;
+	}
+
+	public static boolean checkLicencePlate(ArrayList<Avion> aviones, String matricula) {
+		for (int i = 0; i < aviones.size(); i++) {
+			if (aviones.get(i).getMatricula().equals(matricula))
+				return true;
+		}
+		return false;
+	}
+
+	public static Aeropuerto addPlanesAirport(ArrayList<Avion> aviones1, ArrayList<Avion> aviones2,
+			ArrayList<Avion> aviones3, Aeropuerto aeropuerto) {
+		ArrayList<Avion> planeList = new ArrayList<Avion>(Arrays.asList(aeropuerto.getAviones()));
+		for (int i = 0; i < aviones1.size(); i++)
+			planeList.add(aviones1.get(i));
+		for (int i = 0; i < aviones2.size(); i++)
+			planeList.add(aviones2.get(i));
+		for (int i = 0; i < aviones3.size(); i++)
+			planeList.add(aviones3.get(i));
+		Avion[] newPlanes = new Avion[planeList.size()];
+		newPlanes = planeList.toArray(newPlanes);
+		aeropuerto.setAviones(newPlanes);
+		return aeropuerto;
+	}
+
+	public static ArrayList<Avion> planePointer(ArrayList<Avion> aviones) {
+		return aviones;
+	}
+
+	public static ArrayList<Pasajero> removeValuesPasangers(ArrayList<Pasajero> pasajeros, String surname,
+			char dniLetter) {
+		for (int i = 0; i < pasajeros.size(); i++) {
+			Pasajero pasajero = pasajeros.get(i);
+			if (pasajero.getDni().charAt(pasajero.getDni().length() - 1) == dniLetter)
+				pasajeros.remove(i);
+			String a = pasajero.getApellidos().replace(surname, "");
+			if (pasajero.getApellidos().length() - a.length() != 0)
+				pasajeros.remove(i);
+		}
+		return pasajeros;
 	}
 }
