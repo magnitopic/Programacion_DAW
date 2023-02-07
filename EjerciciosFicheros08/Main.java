c8f0fcf02978382e7b5885b90c5c8c7dfdd8331f
magnitopic <magnitopic@gmail.com>
2023-02-07 16:10:44 +0100

    EjerciciosFicheros8

--- EjerciciosFicheros08/Main.java
+++ EjerciciosFicheros08/Main.java
@@ -1,8 +1,10 @@
 package EjerciciosFicheros08;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -10,6 +12,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		ArrayList<String> data = getCarData();
+		System.out.println(data);
 		if (data.get(0) == null) {
 			System.out.println("No hay datos");
 			return;
@@ -30,46 +33,52 @@ public class Main {
 					System.out.println("Modelo: " + dataValues[2]);
 					System.out.println("Color: " + dataValues[3]);
 					System.out.println("Precio: " + dataValues[4]);
-					System.out.println("Extras: " + dataValues[5]);
 				}
 				break;
 			case 2:
 				newFiles(data);
+				break;
+			case 3:
+				System.out.println("Bye");
 				break;
 		}
 		sc.close();
 	}
 
 	public static void newFiles(ArrayList<String> data) {
-		ArrayList<String> marcas = new ArrayList<String>();
-		for (String d : data) {
-			String[] dataValues = d.split(";");
-			File fichero = new File("EjerciciosFicheros08\\" + dataValues[1] + ".txt");
-			try {
-				if (!fichero.exists()) {
+		try {
+			for (String d : data) {
+				String[] dataValues = d.split(";");
+				File fichero = new File("./EjerciciosFicheros08/" + dataValues[1] + ".txt");
+				if (!fichero.exists())
 					fichero.createNewFile();
-
-				}
-			} catch (IOException e) {
-				System.out.println("Error al crear el fichero");
+				BufferedWriter br = new BufferedWriter(new FileWriter(fichero));
+				br.write(d);
+				br.newLine();
+				br.close();
 			}
+		} catch (IOException e) {
+			System.out.println("Error al crear el fichero");
+		} finally {
+			System.out.println("Ficheros para cada marca creados.");
 		}
 	}
 
 	public static ArrayList<String> getCarData() {
-		File fichero = new File("EjerciciosFicheros08\\MarcaCoche.txt");
+		File fichero = new File("EjerciciosFicheros08/MarcaCoche.txt");
 		ArrayList<String> coches = new ArrayList<String>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(fichero));
 			String str;
-			do {
-				str = br.readLine();
+			while ((str = br.readLine()) != null) {
 				coches.add(str);
-			} while (str != null);
+			}
 			br.close();
 		} catch (Exception e) {
 			System.out.println("Error al leer el fichero");
 		}
+		if (coches.size() == 0)
+			coches.add(null);
 		return coches;
 	}
 }
