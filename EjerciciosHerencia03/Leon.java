be0966c7d17ffaa21dbadea3c07c5fb0861945ac
magnitopic <magnitopic@gmail.com>
2023-03-14 13:56:13 +0100

    PaginaWeb

--- EjerciciosHerencia03/Leon.java
+++ EjerciciosHerencia03/Leon.java
@@ -1,5 +1,9 @@
 package EjerciciosHerencia03;
 
-public class Leon extends Animal{
-	
+public class Leon extends Animal {
+
+	public Leon(String nombre, String procedencia, String altura, String peso) {
+		super(nombre, procedencia, altura, peso);
+	}
+
 }
